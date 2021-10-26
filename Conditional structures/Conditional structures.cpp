21b7f20e49095203204660a8960c21cf97a264c3
Michael Ndigi <38488451+Ndigbiggs@users.noreply.github.com>
2021-10-26 20:50:33 +0300

    Add files via upload

---
+++ Conditional structures/Conditional structures.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+//137307 Michael Ndigi
+int main(){
+	int age;
+		cout<<"Please enter  age\n";
+		cin>>age;
+		
+		if(age>=4){
+			cout<<"Admit to school\n";
+		}else 
+		cout<<"Too young\n";
+		
+		return 0;
+	}
